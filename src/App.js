770d8b73d68fab38ee2524c9d93f2424910e35c6
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-07-23 00:28:09 -0700

    add React Router and nav, main, side, footer routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
 import Sidebar from './components/common/Sidebar';
 import Main from './components/Main';
 import Footer from './components/common/Footer';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Sidebar />
-      <Main />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={() => <Redirect to="/main" />} />
+
+          <Sidebar />
+          <Route exact path="/main" component={() => <Main />} />
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
