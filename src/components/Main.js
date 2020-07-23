9a445d17bd457d41c1b122a0eb2e0e1c764eabde
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-07-22 23:58:16 -0700

    create dummy Main component, delete = bullshit file which I'm pretty sure my cat made

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+//import { Link } from "react-router-dom";
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                HERE'S THE MAIN CUSTOMER LANDING PAGE
+            </div>
+        )
+    }
+}
+
+export default Main;
