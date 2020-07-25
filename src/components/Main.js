bfee394317ca7fa5a425260c63af200641cf5590
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-07-25 09:31:00 -0700

    create store

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
-//import { Link } from "react-router-dom";
+import Footer from './components/common/Footer';
+import Navbar from './components/common/Navbar';
+import Sidebar from './components/common/Sidebar';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Main extends Component {
     render() {
         return (
             <div>
-                HERE'S THE MAIN CUSTOMER LANDING PAGE
+                <Navbar />
+                <Sidebar />
+                <Switch>
+                    <Route exact path="/" component={() => <Redirect to="/main" />} />
+                    <Route exact path="/main" component={() => <Main />} />
+                </Switch>
+                <Footer />
             </div>
         )
     }
 }
 
-export default Main;
+export default withRouter(connect)(Main);
---
+++ src/redux/configureStore.js
@@ -0,0 +1,7 @@
+import { createStore } from 'redux';
+
+export const ConfigureStore = () => {
+    const store = createStore()
+
+    return store;
+};
