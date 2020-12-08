d53b5aa26d783cba0a3230b0405bd93a731832ef
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-12-08 14:40:50 -0800

    fix middlewares

--- src/Root.js
+++ src/Root.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import async from 'middlewares/async';
 import stateValidator from 'middlewares/stateValidator';
+import reduxThunk from 'redux-thunk';
 import reducers from './redux/reducers';
 
 import axios from 'axios';
@@ -12,7 +13,7 @@ export default ({ children, initialState = {} }) => {
     const store = createStore(
         reducers,
         initialState,
-        applyMiddleware(async, stateValidator)
+        applyMiddleware(async, stateValidator, reduxThunk)
     );
     return (<Provider store={store}>{children}</Provider>);
 };
